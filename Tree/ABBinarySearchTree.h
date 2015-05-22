8614545bb851e03f92333b7a6857153a4f186a93
Andrii Bugaiov <andriibugaiov@Senecas-iMac-2.local>
2015-05-22 13:11:10 -0400

    binary tree

---
+++ Tree/ABBinarySearchTree.h
@@ -0,0 +1,13 @@
+//
+//  ABBinarySearchTree.h
+//  workplace
+//
+//  Created by Andrii Bugaiov on 2015-05-22.
+//  Copyright (c) 2015 Andrii Bugaiov. All rights reserved.
+//
+
+#ifndef __workplace__ABBinarySearchTree__
+#define __workplace__ABBinarySearchTree__
+
+
+#endif /* defined(__workplace__ABBinarySearchTree__) */
